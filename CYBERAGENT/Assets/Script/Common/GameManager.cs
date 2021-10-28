9f550a14affa107a258679ea100f56044022011e
UK8ER <uk.821.box@gmail.com>
2021-10-28 20:39:45 +0900

    GameManagerを追加

---
+++ CYBERAGENT/Assets/Script/Common/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    public static class GameManager
+    {
+        public static int _MissionNumber;
+
+    }
+}
