5a9dc28df7fdd2d9ffec4f3d4a6137569615d67b
UK8ER <86774847+UK8ER@users.noreply.github.com>
2021-11-02 11:48:58 +0900

    結果アニメーション表示中に画面操作不可にする制御を追加

--- CYBERAGENT/Assets/Package/Contents/Scripts/View/AnimationView.cs
+++ CYBERAGENT/Assets/Package/Contents/Scripts/View/AnimationView.cs
@@ -16,6 +16,8 @@ namespace Common
         private Text _AnimationText;
         [SerializeField]
         private Transform _AnimationPanel;
+        [SerializeField]
+        private GameObject _BackGroundPanel;
 
         public void SetAnimationText(string text)
         {
@@ -25,7 +27,8 @@ namespace Common
         public void AnimationInit()
         {
             Sequence _Sequence = DOTween.Sequence()
-                .OnStart(() => _AnimatorCanvas.enabled = false)
+                .OnStart(() => _BackGroundPanel.SetActive(false))
+                .AppendCallback(() => _AnimatorCanvas.enabled = false)
                 .Join(_AnimationPanel.DOLocalMove(new Vector3(2160, 0, 0), 0))
                 .AppendCallback(() => _AnimatorCanvas.enabled = true);
         }
@@ -34,12 +37,14 @@ namespace Common
         {
             //_Coroutine = StartCoroutine(StartAnimationAction1());
             Sequence _Sequence = DOTween.Sequence()
-                .OnStart(() => _AnimatorCanvas.enabled = false)
+                .OnStart(() => _BackGroundPanel.SetActive(true))
+                .AppendCallback(() => _AnimatorCanvas.enabled = false)
                 .Join(_AnimationPanel.DOLocalMove(new Vector3(2160, 0, 0), 0))
                 .AppendCallback(() => _AnimatorCanvas.enabled = true)
                 .Join(_AnimationPanel.DOLocalMove(Vector3.zero, 0.5f))
                 .AppendInterval(3.0f)
                 .Append(_AnimationPanel.DOLocalMove(new Vector3(-2160, 0, 0), 0.5f))
+                .AppendCallback(() => _BackGroundPanel.SetActive(false))
                 .AppendCallback(()=>finish());
         }
     }
